80e56201b07750ee28761880a4ebeaee4000ae1c
millisabel <elena.kozlova.job@gmail.com>
2023-04-29 14:43:53 +0200

    feat: add rule import/extensions in ESLint

--- src/script/data/data.js
+++ src/script/data/data.js
@@ -1,479 +1,478 @@
 export const language = 'ru';
 
 export const specKey = {
-	Space: 'Space',
-	Backspace: 'Backspace',
-	Enter: 'Enter',
-	Tab: 'Tab',
-	ArrowLeft: '◄',
-	ArrowUp: '▲',
-	ArrowRight: '►',
-	ArrowDown: '▼',
-	CapsLock: 'CapsLock',
-	ShiftLeft: 'Shift',
-	ShiftRight: 'Shift',
-	ControlLeft: 'CTR',
-	ControlRight: 'CTR',
-	AltLeft: 'ALT',
-	AltRight: 'ALT',
-	MetaLeft: 'Win',
-}
+  Space: 'Space',
+  Backspace: 'Backspace',
+  Enter: 'Enter',
+  Tab: 'Tab',
+  ArrowLeft: '◄',
+  ArrowUp: '▲',
+  ArrowRight: '►',
+  ArrowDown: '▼',
+  CapsLock: 'CapsLock',
+  ShiftLeft: 'Shift',
+  ShiftRight: 'Shift',
+  ControlLeft: 'CTR',
+  ControlRight: 'CTR',
+  AltLeft: 'ALT',
+  AltRight: 'ALT',
+  MetaLeft: 'Win',
+};
 
 export const letter = {
-	en: {
-		Digit1: {
-			key: '1',
-			keyShift: '!',
-		},
-		Digit2: {
-			key: '2',
-			keyShift: '@',
-		},
-		Digit3: {
-			key: '3',
-			keyShift: '#',
-		},
-		Digit4: {
-			key: '4',
-			keyShift: '$',
-		},
-		Digit5: {
-			key: '5',
-			keyShift: '%',
-		},
-		Digit6: {
-			key: '6',
-			keyShift: '^',
-		},
-		Digit7: {
-			key: '7',
-			keyShift: '&',
-		},
-		Digit8: {
-			key: '8',
-			keyShift: '*',
-		},
-		Digit9: {
-			key: '9',
-			keyShift: '(',
-		},
-		Digit0: {
-			key: '0',
-			keyShift: ')',
-		},
-		KeyA: {
-			key: 'a',
-			keyShift: 'A',
-		},
-		KeyB: {
-			key: 'b',
-			keyShift: 'B',
-		},
-		KeyC: {
-			key: 'c',
-			keyShift: 'C',
-		},
-		KeyD: {
-			key: 'd',
-			keyShift: 'D',
-		},
-		KeyE: {
-			key: 'e',
-			keyShift: 'E',
-		},
-		KeyF: {
-			key: 'f',
-			keyShift: 'F',
-		},
-		KeyG: {
-			key: 'g',
-			keyShift: 'G',
-		},
-		KeyH: {
-			key: 'h',
-			keyShift: 'H',
-		},
-		KeyI: {
-			key: 'i',
-			keyShift: 'I',
-		},
-		KeyJ: {
-			key: 'j',
-			keyShift: 'J',
-		},
-		KeyK: {
-			key: 'k',
-			keyShift: 'K',
-		},
-		KeyL: {
-			key: 'l',
-			keyShift: 'L',
-		},
-		KeyM: {
-			key: 'm',
-			keyShift: 'M',
-		},
-		KeyN: {
-			key: 'n',
-			keyShift: 'N',
-		},
-		KeyO: {
-			key: 'o',
-			keyShift: 'O',
-		},
-		KeyP: {
-			key: 'p',
-			keyShift: 'P',
-		},
-		KeyQ: {
-			key: 'q',
-			keyShift: 'Q',
-		},
-		KeyR: {
-			key: 'r',
-			keyShift: 'R',
-		},
-		KeyS: {
-			key: 's',
-			keyShift: 'S',
-		},
-		KeyT: {
-			key: 't',
-			keyShift: 'T',
-		},
-		KeyU: {
-			key: 'u',
-			keyShift: 'U',
-		},
-		KeyV: {
-			key: 'v',
-			keyShift: 'V',
-		},
-		KeyW: {
-			key: 'w',
-			keyShift: 'W',
-		},
-		KeyX: {
-			key: 'x',
-			keyShift: 'X',
-		},
-		KeyY: {
-			key: 'y',
-			keyShift: 'Y',
-		},
-		KeyZ: {
-			key: 'z',
-			keyShift: 'Z',
-		},
-		Minus: {
-			key: '-',
-			keyShift: '_',
-		},
-		Equal: {
-			key: '=',
-			keyShift: '+',
-		},
-		BracketLeft: {
-			key: '[',
-			keyShift: '{',
-		},
-		BracketRight: {
-			key: ']',
-			keyShift: '}',
-		},
-		Backslash: {
-			key: '\\',
-			keyShift: '|',
-		},
-		Backquote: {
-			key: '`',
-			keyShift: '~',
-		},
-		Semicolon: {
-			key: ';',
-			keyShift: ':',
-		},
-		Quote: {
-			key: '\'',
-			keyShift: '\"',
-		},
-		Comma: {
-			key: ',',
-			keyShift: '<',
-		},
-		Period: {
-			key: '.',
-			keyShift: '>',
-		},
-		Slash: {
-			key: '/',
-			keyShift: '?',
-		},
-	},
-	ru: {
-		Digit1: {
-			key: '1',
-			keyShift: '!',
-		},
-		Digit2: {
-			key: '2',
-			keyShift: '"',
-		},
-		Digit3: {
-			key: '3',
-			keyShift: '№',
-		},
-		Digit4: {
-			key: '4',
-			keyShift: ';',
-		},
-		Digit5: {
-			key: '5',
-			keyShift: '%',
-		},
-		Digit6: {
-			key: '6',
-			keyShift: ':',
-		},
-		Digit7: {
-			key: '7',
-			keyShift: '?',
-		},
-		Digit8: {
-			key: '8',
-			keyShift: '*',
-		},
-		Digit9: {
-			key: '9',
-			keyShift: '(',
-		},
-		Digit0: {
-			key: '0',
-			keyShift: ')',
-		},
-		KeyA: {
-			key: 'ф',
-			keyShift: 'Ф',
-		},
-		KeyB: {
-			key: 'и',
-			keyShift: 'И',
-		},
-		KeyC: {
-			key: 'с',
-			keyShift: 'С',
-		},
-		KeyD: {
-			key: 'в',
-			keyShift: 'В',
-		},
-		KeyE: {
-			key: 'у',
-			keyShift: 'У',
-		},
-		KeyF: {
-			key: 'а',
-			keyShift: 'А',
-		},
-		KeyG: {
-			key: 'п',
-			keyShift: 'П',
-		},
-		KeyH: {
-			key: 'р',
-			keyShift: 'Р',
-		},
-		KeyI: {
-			key: 'ш',
-			keyShift: 'Ш',
-		},
-		KeyJ: {
-			key: 'о',
-			keyShift: 'О',
-		},
-		KeyK: {
-			key: 'л',
-			keyShift: 'Л',
-		},
-		KeyL: {
-			key: 'д',
-			keyShift: 'Д',
-		},
-		KeyM: {
-			key: 'ь',
-			keyShift: 'Ь',
-		},
-		KeyN: {
-			key: 'т',
-			keyShift: 'Т',
-		},
-		KeyO: {
-			key: 'щ',
-			keyShift: 'Щ',
-		},
-		KeyP: {
-			key: 'з',
-			keyShift: 'З',
-		},
-		KeyQ: {
-			key: 'й',
-			keyShift: 'Й',
-		},
-		KeyR: {
-			key: 'к',
-			keyShift: 'К',
-		},
-		KeyS: {
-			key: 'ы',
-			keyShift: 'Ы',
-		},
-		KeyT: {
-			key: 'е',
-			keyShift: 'Е',
-		},
-		KeyU: {
-			key: 'г',
-			keyShift: 'Г',
-		},
-		KeyV: {
-			key: 'м',
-			keyShift: 'М',
-		},
-		KeyW: {
-			key: 'ц',
-			keyShift: 'Ц',
-		},
-		KeyX: {
-			key: 'ч',
-			keyShift: 'Ч',
-		},
-		KeyY: {
-			key: 'н',
-			keyShift: 'Н',
-		},
-		KeyZ: {
-			key: 'я',
-			keyShift: 'Я',
-		},
-		Minus: {
-			key: '-',
-			keyShift: '_',
-		},
-		Equal: {
-			key: '=',
-			keyShift: '+',
-		},
-		BracketLeft: {
-			key: 'х',
-			keyShift: 'Х',
-		},
-		BracketRight: {
-			key: 'ъ',
-			keyShift: 'Ъ',
-		},
-		Backslash: {
-			key: '\\',
-			keyShift: '/',
-		},
-		Backquote: {
-			key: 'ё',
-			keyShift: 'Ё',
-		},
-		Semicolon: {
-			key: 'ж',
-			keyShift: 'Ж',
-		},
-		Quote: {
-			key: 'э',
-			keyShift: 'Э',
-		},
-		Comma: {
-			key: 'б',
-			keyShift: 'Б',
-		},
-		Period: {
-			key: 'ю',
-			keyShift: 'Ю',
-		},
-		Slash: {
-			key: '.',
-			keyShift: ',',
-		},
-	}
-}
+  en: {
+    Digit1: {
+      key: '1',
+      keyShift: '!',
+    },
+    Digit2: {
+      key: '2',
+      keyShift: '@',
+    },
+    Digit3: {
+      key: '3',
+      keyShift: '#',
+    },
+    Digit4: {
+      key: '4',
+      keyShift: '$',
+    },
+    Digit5: {
+      key: '5',
+      keyShift: '%',
+    },
+    Digit6: {
+      key: '6',
+      keyShift: '^',
+    },
+    Digit7: {
+      key: '7',
+      keyShift: '&',
+    },
+    Digit8: {
+      key: '8',
+      keyShift: '*',
+    },
+    Digit9: {
+      key: '9',
+      keyShift: '(',
+    },
+    Digit0: {
+      key: '0',
+      keyShift: ')',
+    },
+    KeyA: {
+      key: 'a',
+      keyShift: 'A',
+    },
+    KeyB: {
+      key: 'b',
+      keyShift: 'B',
+    },
+    KeyC: {
+      key: 'c',
+      keyShift: 'C',
+    },
+    KeyD: {
+      key: 'd',
+      keyShift: 'D',
+    },
+    KeyE: {
+      key: 'e',
+      keyShift: 'E',
+    },
+    KeyF: {
+      key: 'f',
+      keyShift: 'F',
+    },
+    KeyG: {
+      key: 'g',
+      keyShift: 'G',
+    },
+    KeyH: {
+      key: 'h',
+      keyShift: 'H',
+    },
+    KeyI: {
+      key: 'i',
+      keyShift: 'I',
+    },
+    KeyJ: {
+      key: 'j',
+      keyShift: 'J',
+    },
+    KeyK: {
+      key: 'k',
+      keyShift: 'K',
+    },
+    KeyL: {
+      key: 'l',
+      keyShift: 'L',
+    },
+    KeyM: {
+      key: 'm',
+      keyShift: 'M',
+    },
+    KeyN: {
+      key: 'n',
+      keyShift: 'N',
+    },
+    KeyO: {
+      key: 'o',
+      keyShift: 'O',
+    },
+    KeyP: {
+      key: 'p',
+      keyShift: 'P',
+    },
+    KeyQ: {
+      key: 'q',
+      keyShift: 'Q',
+    },
+    KeyR: {
+      key: 'r',
+      keyShift: 'R',
+    },
+    KeyS: {
+      key: 's',
+      keyShift: 'S',
+    },
+    KeyT: {
+      key: 't',
+      keyShift: 'T',
+    },
+    KeyU: {
+      key: 'u',
+      keyShift: 'U',
+    },
+    KeyV: {
+      key: 'v',
+      keyShift: 'V',
+    },
+    KeyW: {
+      key: 'w',
+      keyShift: 'W',
+    },
+    KeyX: {
+      key: 'x',
+      keyShift: 'X',
+    },
+    KeyY: {
+      key: 'y',
+      keyShift: 'Y',
+    },
+    KeyZ: {
+      key: 'z',
+      keyShift: 'Z',
+    },
+    Minus: {
+      key: '-',
+      keyShift: '_',
+    },
+    Equal: {
+      key: '=',
+      keyShift: '+',
+    },
+    BracketLeft: {
+      key: '[',
+      keyShift: '{',
+    },
+    BracketRight: {
+      key: ']',
+      keyShift: '}',
+    },
+    Backslash: {
+      key: '\\',
+      keyShift: '|',
+    },
+    Backquote: {
+      key: '`',
+      keyShift: '~',
+    },
+    Semicolon: {
+      key: ';',
+      keyShift: ':',
+    },
+    Quote: {
+      key: '\'',
+      keyShift: '\\"',
+    },
+    Comma: {
+      key: ',',
+      keyShift: '<',
+    },
+    Period: {
+      key: '.',
+      keyShift: '>',
+    },
+    Slash: {
+      key: '/',
+      keyShift: '?',
+    },
+  },
+  ru: {
+    Digit1: {
+      key: '1',
+      keyShift: '!',
+    },
+    Digit2: {
+      key: '2',
+      keyShift: '"',
+    },
+    Digit3: {
+      key: '3',
+      keyShift: '№',
+    },
+    Digit4: {
+      key: '4',
+      keyShift: ';',
+    },
+    Digit5: {
+      key: '5',
+      keyShift: '%',
+    },
+    Digit6: {
+      key: '6',
+      keyShift: ':',
+    },
+    Digit7: {
+      key: '7',
+      keyShift: '?',
+    },
+    Digit8: {
+      key: '8',
+      keyShift: '*',
+    },
+    Digit9: {
+      key: '9',
+      keyShift: '(',
+    },
+    Digit0: {
+      key: '0',
+      keyShift: ')',
+    },
+    KeyA: {
+      key: 'ф',
+      keyShift: 'Ф',
+    },
+    KeyB: {
+      key: 'и',
+      keyShift: 'И',
+    },
+    KeyC: {
+      key: 'с',
+      keyShift: 'С',
+    },
+    KeyD: {
+      key: 'в',
+      keyShift: 'В',
+    },
+    KeyE: {
+      key: 'у',
+      keyShift: 'У',
+    },
+    KeyF: {
+      key: 'а',
+      keyShift: 'А',
+    },
+    KeyG: {
+      key: 'п',
+      keyShift: 'П',
+    },
+    KeyH: {
+      key: 'р',
+      keyShift: 'Р',
+    },
+    KeyI: {
+      key: 'ш',
+      keyShift: 'Ш',
+    },
+    KeyJ: {
+      key: 'о',
+      keyShift: 'О',
+    },
+    KeyK: {
+      key: 'л',
+      keyShift: 'Л',
+    },
+    KeyL: {
+      key: 'д',
+      keyShift: 'Д',
+    },
+    KeyM: {
+      key: 'ь',
+      keyShift: 'Ь',
+    },
+    KeyN: {
+      key: 'т',
+      keyShift: 'Т',
+    },
+    KeyO: {
+      key: 'щ',
+      keyShift: 'Щ',
+    },
+    KeyP: {
+      key: 'з',
+      keyShift: 'З',
+    },
+    KeyQ: {
+      key: 'й',
+      keyShift: 'Й',
+    },
+    KeyR: {
+      key: 'к',
+      keyShift: 'К',
+    },
+    KeyS: {
+      key: 'ы',
+      keyShift: 'Ы',
+    },
+    KeyT: {
+      key: 'е',
+      keyShift: 'Е',
+    },
+    KeyU: {
+      key: 'г',
+      keyShift: 'Г',
+    },
+    KeyV: {
+      key: 'м',
+      keyShift: 'М',
+    },
+    KeyW: {
+      key: 'ц',
+      keyShift: 'Ц',
+    },
+    KeyX: {
+      key: 'ч',
+      keyShift: 'Ч',
+    },
+    KeyY: {
+      key: 'н',
+      keyShift: 'Н',
+    },
+    KeyZ: {
+      key: 'я',
+      keyShift: 'Я',
+    },
+    Minus: {
+      key: '-',
+      keyShift: '_',
+    },
+    Equal: {
+      key: '=',
+      keyShift: '+',
+    },
+    BracketLeft: {
+      key: 'х',
+      keyShift: 'Х',
+    },
+    BracketRight: {
+      key: 'ъ',
+      keyShift: 'Ъ',
+    },
+    Backslash: {
+      key: '\\',
+      keyShift: '/',
+    },
+    Backquote: {
+      key: 'ё',
+      keyShift: 'Ё',
+    },
+    Semicolon: {
+      key: 'ж',
+      keyShift: 'Ж',
+    },
+    Quote: {
+      key: 'э',
+      keyShift: 'Э',
+    },
+    Comma: {
+      key: 'б',
+      keyShift: 'Б',
+    },
+    Period: {
+      key: 'ю',
+      keyShift: 'Ю',
+    },
+    Slash: {
+      key: '.',
+      keyShift: ',',
+    },
+  },
+};
 
 export const keys = [
-	[
-		'Backquote',
-		'Digit1',
-		'Digit2',
-		'Digit3',
-		'Digit4',
-		'Digit5',
-		'Digit6',
-		'Digit7',
-		'Digit8',
-		'Digit9',
-		'Digit0',
-		'Minus',
-		'Equal',
-		'Backspace'
-	],
-	[
-		'Tab',
-		'KeyQ',
-		'KeyW',
-		'KeyE',
-		'KeyR',
-		'KeyT',
-		'KeyY',
-		'KeyU',
-		'KeyI',
-		'KeyO',
-		'KeyP',
-		'BracketLeft',
-		'BracketRight',
-		'Backslash'
-	],
-	[
-		'CapsLock',
-		'KeyA',
-		'KeyS',
-		'KeyD',
-		`KeyF`,
-		'KeyG',
-		'KeyH',
-		'KeyJ',
-		'KeyK',
-		'KeyL',
-		'Semicolon',
-		'Quote',
-		'Enter'],
-	[
-		'ShiftLeft',
-		'KeyZ',
-		'KeyX',
-		'KeyC',
-		'KeyV',
-		'KeyB',
-		'KeyN',
-		'KeyM',
-		'Comma',
-		'Period',
-		'Slash',
-		'ArrowUp',
-		'ShiftRight'
-	],
-	[
-		'ControlLeft',
-		'MetaLeft',
-		'AltLeft',
-		'Space',
-		'AltRight',
-		'ArrowLeft',
-		'ArrowDown',
-		'ArrowRight',
-		'ControlRight'
-	],
+  [
+    'Backquote',
+    'Digit1',
+    'Digit2',
+    'Digit3',
+    'Digit4',
+    'Digit5',
+    'Digit6',
+    'Digit7',
+    'Digit8',
+    'Digit9',
+    'Digit0',
+    'Minus',
+    'Equal',
+    'Backspace',
+  ],
+  [
+    'Tab',
+    'KeyQ',
+    'KeyW',
+    'KeyE',
+    'KeyR',
+    'KeyT',
+    'KeyY',
+    'KeyU',
+    'KeyI',
+    'KeyO',
+    'KeyP',
+    'BracketLeft',
+    'BracketRight',
+    'Backslash',
+  ],
+  [
+    'CapsLock',
+    'KeyA',
+    'KeyS',
+    'KeyD',
+    'KeyF',
+    'KeyG',
+    'KeyH',
+    'KeyJ',
+    'KeyK',
+    'KeyL',
+    'Semicolon',
+    'Quote',
+    'Enter'],
+  [
+    'ShiftLeft',
+    'KeyZ',
+    'KeyX',
+    'KeyC',
+    'KeyV',
+    'KeyB',
+    'KeyN',
+    'KeyM',
+    'Comma',
+    'Period',
+    'Slash',
+    'ArrowUp',
+    'ShiftRight',
+  ],
+  [
+    'ControlLeft',
+    'MetaLeft',
+    'AltLeft',
+    'Space',
+    'AltRight',
+    'ArrowLeft',
+    'ArrowDown',
+    'ArrowRight',
+    'ControlRight',
+  ],
 ];
-
